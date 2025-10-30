30c501922ac14386d5483abb9f1c54f3699c068f
yudigf <yudigf08@gmail.com>
2025-10-30 14:56:31 +0700

    feat: add relationship for development applicants in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,8 @@ class User extends Authenticatable
     {
         return $this->hasOne(FamilyMember::class);
     }
+    public function developmentApplicants()
+    {
+        return $this->hasMany(DevelopmentApplicant::class);
+    }
 }
